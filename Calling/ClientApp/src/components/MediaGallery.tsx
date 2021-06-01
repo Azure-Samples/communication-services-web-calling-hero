576f904ae997f2d84b79ae4f869293ad986afd18
Alex Kwan (ONENOTE) <alkwa@microsoft.com>
2021-06-01 12:58:04 -0700

    only allowing up to 10 participants in the video gallery

--- Calling/ClientApp/src/components/MediaGallery.tsx
+++ Calling/ClientApp/src/components/MediaGallery.tsx
@@ -29,9 +29,11 @@ export default (props: MediaGalleryProps): JSX.Element => {
     console.error('Please use a value for dominante participants between 1 <= x <= 8');
   }
 
-  // we only are going to support up to a 3x3 grid for today (1 local + 8 remote)
-  const rows = [1, 1, 2, 2, 2, 2, 3, 3, 3];
-  const cols = [1, 2, 2, 2, 3, 3, 3, 3, 3];
+  // For now we are only going to support up to a 4x3 grid or 10 participants in a call
+  // Since this is a sample, we will just hard-code how we want the grid to scale
+  // the rows and columns for the number of users in the call
+  const rows          = [1, 1, 2, 2, 2, 2, 3, 3, 3, 3];
+  const cols          = [1, 2, 2, 2, 3, 3, 3, 3, 3, 4];
 
   const calculateNumberOfRows = React.useCallback((participants, maxStreamsToRender) => {
     const length = Math.min(participants.length, maxStreamsToRender);

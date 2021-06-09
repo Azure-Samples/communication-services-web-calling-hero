b6ede5992aecfb9110f5dd12d8df0653749cfe35
Ravi Thanneeru <v-ratann@microsoft.com>
2021-06-08 19:54:21 -0700

    Formatting with prettier

--- Calling/ClientApp/src/containers/Header.ts
+++ Calling/ClientApp/src/containers/Header.ts
@@ -6,39 +6,39 @@ import { Constants } from '../core/constants';
 import { State } from '../core/reducers';
 
 const mapStateToProps = (state: State, props: HeaderProps) => ({
-    actionable:
-        state.calls.callState === Constants.INCOMING ||
-        state.calls.callState === Constants.RINGING ||
-        state.calls.callState === Constants.CONNECTING ||
-        state.calls.callState === Constants.CONNECTED,
-    mic: state.controls.mic,
-    call: state.calls.call,
-    shareScreen: state.controls.shareScreen,
-    endCall: (): void => {
-        state.calls.call && endCall(state.calls.call, { forEveryone: false });
-        props.endCallHandler();
-    },
-    videoDeviceInfo: state.devices.videoDeviceInfo,
-    screenWidth: props.screenWidth,
-    screenShareStreams: state.streams.screenShareStreams,
-    localVideoRendererIsBusy: state.streams.localVideoRendererIsBusy,
-    cameraPermission: state.devices.cameraPermission,
-    microphonePermission: state.devices.microphonePermission,
-    recordingId: state.calls.recordingId,
-    recordingStatus: state.calls.recordingStatus
+  actionable:
+    state.calls.callState === Constants.INCOMING ||
+    state.calls.callState === Constants.RINGING ||
+    state.calls.callState === Constants.CONNECTING ||
+    state.calls.callState === Constants.CONNECTED,
+  mic: state.controls.mic,
+  call: state.calls.call,
+  shareScreen: state.controls.shareScreen,
+  endCall: (): void => {
+    state.calls.call && endCall(state.calls.call, { forEveryone: false });
+    props.endCallHandler();
+  },
+  videoDeviceInfo: state.devices.videoDeviceInfo,
+  screenWidth: props.screenWidth,
+  screenShareStreams: state.streams.screenShareStreams,
+  localVideoRendererIsBusy: state.streams.localVideoRendererIsBusy,
+  cameraPermission: state.devices.cameraPermission,
+  microphonePermission: state.devices.microphonePermission,
+  recordingId: state.calls.recordingId,
+  recordingStatus: state.calls.recordingStatus
 });
 
 const mapDispatchToProps = (dispatch: any) => ({
-    setMic: (mic: boolean): void => dispatch(setMicrophone(mic)),
-    setScreenShare: (screenShare: boolean): void => dispatch(setShareUnshareScreen(screenShare)),
-    resetRecordingStatus: () => dispatch(resetRecordingState()),
-    // Only support Desktop -- Chrome | Edge (Chromium) | Safari
-    isLocalScreenShareSupportedInBrowser: (): boolean => {
-        return (
-            !utils.isMobileSession() &&
-            (/chrome/i.test(navigator.userAgent.toLowerCase()) || /safari/i.test(navigator.userAgent.toLowerCase()))
-        );
-    }
+  setMic: (mic: boolean): void => dispatch(setMicrophone(mic)),
+  setScreenShare: (screenShare: boolean): void => dispatch(setShareUnshareScreen(screenShare)),
+  resetRecordingStatus: () => dispatch(resetRecordingState()),
+  // Only support Desktop -- Chrome | Edge (Chromium) | Safari
+  isLocalScreenShareSupportedInBrowser: (): boolean => {
+    return (
+      !utils.isMobileSession() &&
+      (/chrome/i.test(navigator.userAgent.toLowerCase()) || /safari/i.test(navigator.userAgent.toLowerCase()))
+    );
+  }
 });
 
 const connector: any = connect(mapStateToProps, mapDispatchToProps);

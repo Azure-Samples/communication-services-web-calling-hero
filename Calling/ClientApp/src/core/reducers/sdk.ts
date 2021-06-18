4d32f209ca525a085a7b4ec884c878d1303ff53d
alkwa-msft <79329532+alkwa-msft@users.noreply.github.com>
2021-06-18 11:10:25 -0700

    Alkwa/use one call client (#71)
    * use one call client
    
    * useCallback instead of useMemo for the handler
    
    * prettier fixes

--- Calling/ClientApp/src/core/reducers/sdk.ts
+++ Calling/ClientApp/src/core/reducers/sdk.ts
@@ -1,8 +1,10 @@
+import { CallClient } from '@azure/communication-calling';
 import { Reducer } from 'redux';
-import { SET_USERID, SdkTypes, SET_DISPLAY_NAME } from '../actions/sdk';
+import { SET_USERID, SdkTypes, SET_CALL_CLIENT, SET_DISPLAY_NAME } from '../actions/sdk';
 
 export interface SdkState {
   userId?: string;
+  callClient?: CallClient;
   displayName: string;
 }
 
@@ -14,6 +16,8 @@ export const sdkReducer: Reducer<SdkState, SdkTypes> = (state = initialState, ac
   switch (action.type) {
     case SET_USERID:
       return { ...state, userId: action.userId };
+    case SET_CALL_CLIENT:
+      return { ...state, callClient: action.callClient };
     case SET_DISPLAY_NAME:
       return { ...state, displayName: action.displayName };
     default:

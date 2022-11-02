0f042b4a2c2b212b062b185e89cf47ab90da38a5
Prathmesh Prabhu <82062616+prprabhu-ms@users.noreply.github.com>
2022-11-02 18:38:45 +0000

    Fix teams interop call initiation

--- Calling/src/app/App.tsx
+++ Calling/src/app/App.tsx
@@ -90,7 +90,7 @@ const App = (): JSX.Element => {
           startCallHandler={async (callDetails) => {
             setDisplayName(callDetails.displayName);
 
-            let callLocator: CallAdapterLocator | undefined = getTeamsLinkFromUrl() || getGroupIdFromUrl();
+            let callLocator: CallAdapterLocator | undefined = callDetails.callLocator || getTeamsLinkFromUrl() || getGroupIdFromUrl();
 
             callLocator = callLocator || createGroupId();
 

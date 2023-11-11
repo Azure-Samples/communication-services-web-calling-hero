b9bb7afd972acd773b066ffd6b77a8884758c441
alkwa-msft <79329532+alkwa-msft@users.noreply.github.com>
2023-11-10 16:54:52 -0800

    updating calling sample to use 1.10.0 (#210)
    * updating calling sample
    
    * fixing updat sample changes
    
    * removing additional artifacts from the porting process
    
    * removing artifacts from cc process
    
    * adding in back some of the hooks
    
    * making changes to reduce unnecessary changes to updated sample
    
    * fixing lint issues

--- Calling/src/index.tsx
+++ Calling/src/index.tsx
@@ -6,7 +6,6 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './app/App';
 import { SwitchableFluentThemeProvider } from './app/theming/SwitchableFluentThemeProvider';
-import { ShakeToSendLogs } from './app/utils/ShakeToSendLogs';
 
 const domNode = document.getElementById('root');
 if (!domNode) {
@@ -14,10 +13,11 @@ if (!domNode) {
 }
 
 createRoot(domNode).render(
-  <SwitchableFluentThemeProvider scopeId="SampleCallingApp">
-    <div className="wrapper">
-      <App />
-      <ShakeToSendLogs />
-    </div>
-  </SwitchableFluentThemeProvider>
+  <React.StrictMode>
+    <SwitchableFluentThemeProvider scopeId="SampleCallingApp">
+      <div className="wrapper">
+        <App />
+      </div>
+    </SwitchableFluentThemeProvider>
+  </React.StrictMode>
 );

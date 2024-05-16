306ac7423c534fef0c4ff7383dc34ba1875c99d8
carocao-msft <96077406+carocao-msft@users.noreply.github.com>
2024-05-16 15:51:58 +0000

    remove

--- Calling/src/index.tsx
+++ Calling/src/index.tsx
@@ -6,14 +6,11 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './app/App';
 import { SwitchableFluentThemeProvider } from './app/theming/SwitchableFluentThemeProvider';
-import { initReactRenderTracker } from './app/utils/AppUtils';
 
 const domNode = document.getElementById('root');
 if (!domNode) {
   throw new Error('Failed to find the root element');
 }
-
-initReactRenderTracker();
 
 createRoot(domNode).render(
   <React.StrictMode>

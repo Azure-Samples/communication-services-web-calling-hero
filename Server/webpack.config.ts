48c2699a402463d8c18728acec1075a5ccec556b
Abrar Khan <saytoabrarkhan@gmail.com>
2024-10-28 18:02:52 +0530

    test

--- Server/webpack.config.ts
+++ Server/webpack.config.ts
@@ -1,8 +1,11 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
-
 import path from 'path';
+import { fileURLToPath } from 'url';
 import CopyWebpackPlugin from 'copy-webpack-plugin';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const config = {
   name: 'server',
@@ -29,9 +32,15 @@ const config = {
   },
   plugins: [
     new CopyWebpackPlugin({
-      patterns: [{ from: path.join(__dirname, 'web.config'), to: path.join(__dirname, 'dist') }]
+      patterns: [
+        { from: path.join(__dirname, 'web.config'), to: path.join(__dirname, 'dist') },
+        { from: path.join(__dirname, 'public'), to: path.join(__dirname, 'dist/public') }
+      ]
     })
   ]
 };
+
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
 
 export default config;
